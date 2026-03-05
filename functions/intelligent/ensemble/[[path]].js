3fa7cc56c7ce06ef2b3e497f3b10aa63b3a47b20
zjz <mac_mini_de_zjz@zjzs-Mac-mini.local>
2026-03-04 19:05:24 -0600

    route: hard-block /intelligent/ensemble via edge 404 handlers

---
+++ functions/intelligent/ensemble/[[path]].js
@@ -0,0 +1,8 @@
+export const onRequest = async () =>
+  new Response("Not found", {
+    status: 404,
+    headers: {
+      "content-type": "text/plain; charset=utf-8",
+      "cache-control": "no-store"
+    }
+  });
